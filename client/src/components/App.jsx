3998525bced5d5456e073f627a4d708305e01b78
Erik <elin7780@bths.edu>
2018-08-31 19:01:54 -0400

    Begin work on creating framework for frontend components

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import Home from './Home.jsx'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    )
+  }
+}
+
+export default App
